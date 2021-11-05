9adcb7b4912cae59fee27e8e725ea812c5d22184
agustintosco <agustintosco.92@gmail.com>
2021-11-05 18:50:56 +0100

    added twit.js file with config

---
+++ twit.js
@@ -0,0 +1,8 @@
+import Twit from "twit";
+
+export const T = new Twit({
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
